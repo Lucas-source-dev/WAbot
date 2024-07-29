d900cdd70189ba93d15feeafc2211777ab0820e3
lucas <esempre.ele1@gmail.com>
2024-07-29 11:20:50 -0300

    Update project

---
+++ generate.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+const DEFAULT_PARAMS = {
+  model: "text-davinci-003",
+  temperature: 0.7,
+  max_tokens: 1000,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+  stop: [" Human:", " AI:"],
+};
+
+async function query(params = {}) {
+  const params_ = { ...DEFAULT_PARAMS, ...params };
+
+  const requestOptions = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "",
+    },
+  };
+  try {
+    const response = await axios.post("https://api.openai.com/v1/completions", params_, requestOptions);
+    return response.data.choices[0].text;
+  } catch (err) {
+    return "error";
+  }
+}
+
+module.exports = query;
